fb51c9841aff1942b0c385ecf288d73cb990c714
VictorDemerji <demerji.v1@gmail.com>
2025-09-20 13:15:37 +0300

    Change First class

--- src/First.java
+++ src/First.java
@@ -1,7 +1,18 @@
 public class First {
     public static void main(String[] args) {
+        displayAddress();
+        displayAddress();
+        anatolMelnic();
+    }
+
+    public static void displayAddress() {
         System.out.println("Compania ABC");
         System.out.println("str. Columne 140, MD-2004");
-        System.out.println("Chisinau, Republica Moldova");
+        System.out.println("Chisinau, Republica Moldova\n");
+
+    }
+
+    public static void anatolMelnic(){
+        System.out.println("1.Zeama de gaina \n2. Mamaliga \n3. Compot");
     }
 }
