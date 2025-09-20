fb6ed4f6758da54e463891f7077312abada2a989
VictorDemerji <demerji.v1@gmail.com>
2025-09-20 13:03:36 +0300

    First class

---
+++ src/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Compania ABC");
+        System.out.println("str. Columne 140, MD-2004");
+        System.out.println("Chisinau, Republica Moldova");
+    }
+}
